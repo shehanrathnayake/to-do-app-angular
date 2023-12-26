74db4f8ec6e7569f44cfabdd340a42550d7101b6
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-12-26 13:15:10 +0530

    Update Readme file

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## Version
 0.1.0
+
+## Sample Images
+
+<img src="demo/todo-app-light.png" width="300px">
+
+<img src="demo/todo-app-dark.png" width="300px">
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -5,11 +5,11 @@ import {Injectable} from "@angular/core";
 export class TaskService {
 
   private taskList: Array<TaskDto> = [
-    new TaskDto(1, 'Design wireframes'),
-    new TaskDto(2, 'Setup project', true),
-    new TaskDto(3, 'Contact HR'),
-    new TaskDto(4, 'Push `abc` branch to `dev` repo'),
-    new TaskDto(5, 'Test customer service', true)
+    new TaskDto(1, 'Design user interface'),
+    new TaskDto(2, 'Setup database connection', true),
+    new TaskDto(3, 'Buy a domain'),
+    new TaskDto(4, 'Push all the workings to GitHub'),
+    new TaskDto(5, 'Review the app', true)
   ];
   addTask(task: TaskDto) {
     this.taskList.push(task);
